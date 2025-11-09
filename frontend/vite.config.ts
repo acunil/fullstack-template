11f960fec6eb37c16c3dfefb351f3ea2da99f1b4
lucian <locamos@gmail.com>
2025-11-09 16:38:41 +0000

    Splits docker and local configs to enable local testing and run without changing settings for docker deployments

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/test': {
-        target: 'http://backend:8080',
-        changeOrigin: true,
-        secure: false,
+export default ({ mode }) => {
+  // load .env, .env.local, .env.[mode] etc
+  const env = loadEnv(mode, process.cwd(), '')
+
+  // Prefer VITE_BACKEND_URL, fallback to localhost
+  const backendUrl = env.VITE_BACKEND_URL || 'http://localhost:8080'
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/test': {
+          target: backendUrl,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-})
+  })
+}
